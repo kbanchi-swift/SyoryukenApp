858038ee19af8df2cc236540bee8b5171fb86579
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-03 16:11:55 +0900

    指摘対応

--- SyoryukenApp/ViewController.swift
+++ SyoryukenApp/ViewController.swift
@@ -9,27 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let image_names = ["attak1", "attak2", "attak3", "attak4", "attak5", "attak6", "attak7", "attak8", "attak9", "attak10", "attak11", "attak12", "attak13", "attak14", "attak15", "attak16", "attak17", "attak18", "attak19", "attak20"]
+    let imageNames = ["attak1", "attak2", "attak3", "attak4", "attak5", "attak6", "attak7", "attak8", "attak9", "attak10", "attak11", "attak12", "attak13", "attak14", "attak15", "attak16", "attak17", "attak18", "attak19", "attak20"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        imageView.image = UIImage(named: image_names[0])
+        imageView.image = UIImage(named: imageNames[0])
     }
 
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func tappedButton(_ sender: Any) {
-        imageView.animationImages = animatedImages(for: image_names)
+        imageView.animationImages = animatedImages(for: imageNames)
         imageView.animationDuration = 1
         imageView.animationRepeatCount = 1
         imageView.startAnimating()
     }
     
-    func animatedImages(for image_names: [String]) -> [UIImage] {
+    func animatedImages(for imageNames: [String]) -> [UIImage] {
         var images:[UIImage] = []
-        for image_name in image_names {
-            if let image = UIImage(named: image_name) {
+        for imageName in imageNames {
+            if let image = UIImage(named: imageName) {
                 images.append(image)
             }
         }
